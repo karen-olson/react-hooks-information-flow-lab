89ed368402cc73638f9df62ee92229a98c225f83
Karen Olson <karmarie@gmail.com>
2021-08-31 11:32:48 -0500

    commit tree a7785844683d69e9f54e975cf3e396633a172eb3

--- src/index.js
+++ src/index.js
@@ -4,3 +4,40 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+/*
+
+Step 1: Component Hierarchy
+
+  App
+    Header
+    ShoppingList
+        Filter
+        Item
+        Item
+        Item
+
+
+Step 2: Static Version of app
+    Done
+
+
+Step 3: Which state variables do we need?
+
+    isDarkMode
+    isInCart
+    selectedCategory
+
+
+Step 4: Where should state live?
+
+    What components render something based on this state?
+        isDarkMode: Header, App
+        isInCart: 
+        selectedCategory: 
+
+    What is the common owner component?
+        isDarkMode: App
+
+
+*/
